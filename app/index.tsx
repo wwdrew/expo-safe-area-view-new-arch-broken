6251e094ff04d4096d05be688026a8f113187e27
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-07-04 12:24:23 +0100

    remove redundant files

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,24 @@
 import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+        <EditScreenInfo path="app/(tabs)/index.tsx" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
